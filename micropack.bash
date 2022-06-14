12dbd35571bab5cc6d81ead91438005c6e4a72d8
Nicolas Biri <nicolas.biri@list.lu>
2022-06-14 08:15:36 +0200

    tweaking the install for racket-scheme and m1

--- micropack.bash
+++ micropack.bash
@@ -5,6 +5,6 @@ set -eux
 git clone https://github.com/idris-lang/Idris2.git idris2
 pushd idris2
 make support
-cp -v support/c/libidris2_support.so ../micropack/
+cp -v support/c/libidris2_support.dylib ../micropack/
 popd
 rm -rf idris2
