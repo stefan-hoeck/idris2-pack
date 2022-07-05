56db72861d879867a447b06ced668370031d7ebc
stefan-hoeck <hock@zhaw.ch>
2022-07-05 21:34:02 +0200

    [ version ] fix script

--- version.sh
+++ version.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-GIT_SHA1=$(git rev-parse HEAD)
+GIT_SHA1=""
+
+git status > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  GIT_SHA1=$(git rev-parse HEAD)
+fi
 
 echo "version = \"$GIT_SHA1\"" >> src/Pack/Version.idr
