e9ad6ac041f3f77bf5be47e6bc14d89ddcefafb9
stefan-hoeck <hock@zhaw.ch>
2022-07-05 21:46:17 +0200

    [ CI ] trickier than I thought

---
+++ restore.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+if [ $(git status > /dev/null 2>&1; echo $?) -eq 0 ]; then
+  git restore src/Pack/Version.idr
+fi
--- version.sh
+++ version.sh
@@ -2,9 +2,7 @@
 
 GIT_SHA1=""
 
-git status > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
+if [ $(git status > /dev/null 2>&1; echo $?) -eq 0 ]; then
   GIT_SHA1=$(git rev-parse HEAD)
 fi
 
