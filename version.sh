347cf2b1cd46a8dba2163e3abd3745aad5a35f71
stefan-hoeck <hock@zhaw.ch>
2022-07-05 20:40:24 +0200

    [ version ] cleanup script

--- version.sh
+++ version.sh
@@ -2,8 +2,4 @@
 
 GIT_SHA1=$(git rev-parse HEAD)
 
-echo "-- @""generated" > src/Pack/Version.idr
-echo "module Pack.Version" >> src/Pack/Version.idr
-echo "export" >> src/Pack/Version.idr
-echo "version : String" >> src/Pack/Version.idr
 echo "version = \"$GIT_SHA1\"" >> src/Pack/Version.idr
