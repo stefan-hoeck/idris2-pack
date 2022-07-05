3681fe4fd00aa890e16c89322dd290c0a3d6f3a2
stefan-hoeck <hock@zhaw.ch>
2022-07-05 21:50:53 +0200

    [ lint ] and now the linter

--- restore.sh
+++ restore.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-if [ $(git status > /dev/null 2>&1; echo $?) -eq 0 ]; then
+if [ $(
+    git status >/dev/null 2>&1
+    echo $?
+) -eq 0 ]; then
   git restore src/Pack/Version.idr
 fi
--- version.sh
+++ version.sh
@@ -2,8 +2,11 @@
 
 GIT_SHA1=""
 
-if [ $(git status > /dev/null 2>&1; echo $?) -eq 0 ]; then
-  GIT_SHA1=$(git rev-parse HEAD)
+if [ $(
+    git status >/dev/null 2>&1
+    echo $?
+) -eq 0 ]; then
+    GIT_SHA1=$(git rev-parse HEAD)
 fi
 
-echo "version = \"$GIT_SHA1\"" >> src/Pack/Version.idr
+echo "version = \"$GIT_SHA1\"" >>src/Pack/Version.idr
