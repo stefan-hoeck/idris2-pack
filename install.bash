9515939e6a1bbb658036451dd28400d1a75ba2a0
stefan-hoeck <hock@zhaw.ch>
2025-10-12 16:43:14 +0200

    [ lint ] more indentation in here-doc

--- install.bash
+++ install.bash
@@ -221,69 +221,69 @@ EOF
 
 if [ ! -f "$USER_DIR/pack.toml" ]; then
 	cat <<-EOF >>"$USER_DIR/pack.toml"
-	[install]
+		[install]
 
-	# Whether to install packages together with their
-	# sources or not. This is mainly useful for programmers
-	# who have set their editor up with some *go to definition*
-	# functionality (for instance by using idris2-lsp with neovim).
-	with-src   = true
+		# Whether to install packages together with their
+		# sources or not. This is mainly useful for programmers
+		# who have set their editor up with some *go to definition*
+		# functionality (for instance by using idris2-lsp with neovim).
+		with-src   = true
 
-	# Whether to prompt the user before building or installing
-	# packages or applications with custom build hooks in their
-	# \`.ipkg\` file.
-	safety-prompt = true
+		# Whether to prompt the user before building or installing
+		# packages or applications with custom build hooks in their
+		# \`.ipkg\` file.
+		safety-prompt = true
 
-	# Must-have libraries. These will be installed automatically
-	# when using a new package collection.
-	# libs       = [ "toml", "elab-util" ]
+		# Must-have libraries. These will be installed automatically
+		# when using a new package collection.
+		# libs       = [ "toml", "elab-util" ]
 
-	# Must-have applications. These will be installed automatically
-	# when using a new package collection.
-	# apps       = [ "idris2-lsp" ]
+		# Must-have applications. These will be installed automatically
+		# when using a new package collection.
+		# apps       = [ "idris2-lsp" ]
 
-	[idris2]
+		[idris2]
 
-	# Whether to build Idris2 with its bootstrap compiler.
-	# Bootstrapping takes longer than building with an existing
-	# Idris2 installation, but it will work even if the existing
-	# Idris2 compiler is outdated.
-	bootstrap  = false
+		# Whether to build Idris2 with its bootstrap compiler.
+		# Bootstrapping takes longer than building with an existing
+		# Idris2 installation, but it will work even if the existing
+		# Idris2 compiler is outdated.
+		bootstrap  = false
 
-	# Name or path to the scheme executable to use.
-	scheme      = "$SCHEME"
+		# Name or path to the scheme executable to use.
+		scheme      = "$SCHEME"
 
-	# Default code generator to us
-	# codegen     = "chez"
+		# Default code generator to us
+		# codegen     = "chez"
 
-	# Set this to \`true\` in order to run REPL sessions from within
-	# \`rlwrap\`. This will give you additional features such as a
-	# command history.
-	repl.rlwrap = false
+		# Set this to \`true\` in order to run REPL sessions from within
+		# \`rlwrap\`. This will give you additional features such as a
+		# command history.
+		repl.rlwrap = false
 
-	# Below are some examples for custom packages
+		# Below are some examples for custom packages
 
-	# A local package to be available with all
-	# package collections.
-	# [custom.all.chem]
-	# type = "local"
-	# path = "/data/idris/chem"
-	# ipkg = "chem.ipkg"
+		# A local package to be available with all
+		# package collections.
+		# [custom.all.chem]
+		# type = "local"
+		# path = "/data/idris/chem"
+		# ipkg = "chem.ipkg"
 
-	# A package on GitHub to be available with all
-	# package collections.
-	# [custom.all.foo]
-	# type = "github"
-	# path = "https://github.com/bar/foo"
-	# ipkg = "foo.ipkg"
+		# A package on GitHub to be available with all
+		# package collections.
+		# [custom.all.foo]
+		# type = "github"
+		# path = "https://github.com/bar/foo"
+		# ipkg = "foo.ipkg"
 
-	# Override library \`toml\` from package collection \`nightly-220503\`
-	# by using a custom commit hash.
-	# [custom.nightly-220503.toml]
-	# type   = "github"
-	# url    = "https://github.com/cuddlefishie/toml-idr"
-	# commit = "eb7a146f565276f82ebf30cb6d5502e9f65dcc3c"
-	# ipkg   = "toml.ipkg"
+		# Override library \`toml\` from package collection \`nightly-220503\`
+		# by using a custom commit hash.
+		# [custom.nightly-220503.toml]
+		# type   = "github"
+		# url    = "https://github.com/cuddlefishie/toml-idr"
+		# commit = "eb7a146f565276f82ebf30cb6d5502e9f65dcc3c"
+		# ipkg   = "toml.ipkg"
 	EOF
 fi
 
