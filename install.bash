7fa50d7c1ef8faeb23d1c55476a0b9e200f3d678
Stefan Höck <hock@zhaw.ch>
2025-10-25 15:35:27 +0000

    [ fix ] use default settings in pregenerated toml files (#351)
    * [ fix ] use default settings in pregenerated toml files
    
    * [ fix ] use SCHEME variable in bash script
    
    * [ micropack ] update installers
    
    * [ fix ] missing backtick
    
    * [ micropack ] update installers
    
    * [ fix ] replace backticks with single quotes in heredoc

--- install.bash
+++ install.bash
@@ -234,20 +234,55 @@ if [ ! -f "$USER_DIR/pack.toml" ]; then
 		# sources or not. This is mainly useful for programmers
 		# who have set their editor up with some *go to definition*
 		# functionality (for instance by using idris2-lsp with neovim).
-		with-src   = true
+		# with-src   = true
+
+		# Whether to install API docs together with installed
+		# libraries.
+		# with-docs = false
+
+		# Whether to use katla to generate HTML links to
+		# semantically highlighted API sources
+		# use-katla = false
 
 		# Whether to prompt the user before building or installing
 		# packages or applications with custom build hooks in their
-		# \`.ipkg\` file.
-		safety-prompt = true
+		# '.ipkg' file.
+		# safety-prompt = true
+
+		# Whether to prompt the user before running the garbage collector
+		# via command 'gc'.
+		# gc-prompt = true
+
+		# Whether to remove all outdated libraries and applications during
+		# garbage collection (command 'gc').
+		# gc-purge = false
+
+		# Whether to issue a warning in presence of a local 'depends' directory
+		# which might interfere with the libraries managed by pack
+		# warn-depends = true
+
+		# List of packages and apps with custom build hooks we trust to
+		# be safe. This gives more fine grained control over package safety
+		# than 'safety-prompt'.
+		# whitelist = [ "pack", "idris2-lsp" ]
 
 		# Must-have libraries. These will be installed automatically
 		# when using a new package collection.
-		# libs       = [ "toml", "elab-util" ]
+		# Example: 'libs       = [ "toml", "elab-util" ]'
+		# libs       = []
 
 		# Must-have applications. These will be installed automatically
 		# when using a new package collection.
-		# apps       = [ "idris2-lsp" ]
+		# Example: 'apps       = [ "idris2-lsp" ]'
+		# apps       = []
+
+		[pack]
+
+		# Override this to use a custom Git repo for pack
+		# url = "https://github.com/stefan-hoeck/idris2-pack"
+
+		# Override this to use a custom commit and branch for pack
+		# commit = "latest:main"
 
 		[idris2]
 
@@ -255,7 +290,7 @@ if [ ! -f "$USER_DIR/pack.toml" ]; then
 		# Bootstrapping takes longer than building with an existing
 		# Idris2 installation, but it will work even if the existing
 		# Idris2 compiler is outdated.
-		bootstrap  = false
+		# bootstrap  = true
 
 		# Name or path to the scheme executable to use.
 		scheme      = "$SCHEME"
@@ -263,34 +298,107 @@ if [ ! -f "$USER_DIR/pack.toml" ]; then
 		# Default code generator to us
 		# codegen     = "chez"
 
-		# Set this to \`true\` in order to run REPL sessions from within
-		# \`rlwrap\`. This will give you additional features such as a
+		# Set this to 'true' in order to run REPL sessions from within
+		# 'rlwrap'. This will give you additional features such as a
 		# command history.
-		repl.rlwrap = false
+		# Alternatively, you can pass additional command-line arguments
+		# to 'rlwrap' by setting this to a string or an array of strings,
+		# e.g. to "-pGreen -aN" or ["-pGreen", "--no-children"].
+		# repl.rlwrap = false
+
+		# Packages to load automatically when starting a REPL session
+		# without an '.ipkg' file in scope. This defaults to "none".
+		# Note: Uncomment only one of the following examples:
+		# repl.autoload   = "installed"
+		# repl.autoload   = "autolibs"
+		# repl.autoload   = [ "sop", "toml" ]
+
+		# Override this to use a custom Git repo for the Idris compiler
+		# url = "https://github.com/idris-lang/Idris2"
+
+		# Override this to use a custom commit and branch for the Idris compiler
+		# commit = "latest:main"
+
+		# Override this to have a .gitignore file and .git directory
+		# created inside of a new pack project upon its creation.
+		# git = false
+
+		# Additional arguments to be passed to the Idris compiler
+		# This can be a single string or a list of strings.
+		# extra-args = []
 
 		# Below are some examples for custom packages
 
 		# A local package to be available with all
-		# package collections.
+		# package collections. The path to the package's root
+		# directory can be absolute or relative. In the latter
+		# case, it will be considered to be relative to the
+		# parent directory of the 'pack.toml' file where it is
+		# defined.
+		#
+		# The path to '.ipkg' files must always relative to the
+		# given 'path'.
 		# [custom.all.chem]
 		# type = "local"
 		# path = "/data/idris/chem"
 		# ipkg = "chem.ipkg"
 
-		# A package on GitHub to be available with all
+		# A package as a Git repository to be available with all
 		# package collections.
 		# [custom.all.foo]
-		# type = "github"
-		# path = "https://github.com/bar/foo"
+		# type = "git"
+		# url  = "https://github.com/bar/foo"
 		# ipkg = "foo.ipkg"
 
-		# Override library \`toml\` from package collection \`nightly-220503\`
+		# Override library 'toml' from package collection 'nightly-220503'
 		# by using a custom commit hash.
-		# [custom.nightly-220503.toml]
-		# type   = "github"
-		# url    = "https://github.com/cuddlefishie/toml-idr"
+		# [custom.nightly-251023.ilex]
+		# type   = "git"
+		# url    = "https://github.com/stefan-hoeck/idirs2-ilex"
 		# commit = "eb7a146f565276f82ebf30cb6d5502e9f65dcc3c"
-		# ipkg   = "toml.ipkg"
+		# ipkg   = "ilex.ipkg"
+
+		# Uncomment and adjust the following entries to specify the
+		# default log level associated with each pack command.
+		# Possible log levels are:
+		#   "debug"
+		#   "build"
+		#   "info"
+		#   "cache"
+		#   "warning"
+		#   "silence"
+		[log]
+
+		# build             = "build"
+		# install-deps      = "build"
+		# typecheck         = "build"
+		# clean             = "build"
+		# cleanbuild        = "build"
+		# repl              = "warning"
+		# exec              = "warning"
+		# install           = "build"
+		# install-app       = "build"
+		# remove            = "build"
+		# remove-app        = "build"
+		# run               = "warning"
+		# test              = "warning"
+		# new               = "build"
+		# update            = "build"
+		# fetch             = "build"
+		# package-path      = "silence"
+		# libs-path         = "silence"
+		# data-path         = "silence"
+		# app-path          = "silence"
+		# switch            = "build"
+		# update-db         = "build"
+		# gc                = "info"
+		# info              = "cache"
+		# query             = "cache"
+		# fuzzy             = "cache"
+		# completion        = "silence"
+		# completion-script = "silence"
+		# uninstall         = "info"
+		# help              = "silence"
 	EOF
 fi
 
