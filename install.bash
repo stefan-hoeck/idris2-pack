7f27772c121cdf49b5aab8b62453b5800adc61d2
stefan-hoeck <hock@zhaw.ch>
2026-09-06 12:56:06 +0200

    [ fix ] install the enum library as part of the install.bash script

--- install.bash
+++ install.bash
@@ -170,6 +170,13 @@ pushd "$CLONES_DIR/idris2-literal"
 "$BOOT_PATH" --install literal.ipkg
 popd
 
+# Install enum
+
+git clone --depth=1 https://github.com/stefan-hoeck/idris2-enum.git "$CLONES_DIR/idris2-enum"
+pushd "$CLONES_DIR/idris2-enum"
+"$BOOT_PATH" --install enum.ipkg
+popd
+
 # Install ilex-core, ilex, and ilex-toml
 
 git clone --depth=1 https://github.com/stefan-hoeck/idris2-ilex.git "$CLONES_DIR/idris2-ilex"
